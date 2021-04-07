51169a5461876094e9aacc1817287e818245abe7
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-07 10:12:19 +1000

    Update Easy-ReverseInteger.py

--- Easy-ReverseInteger.py
+++ Easy-ReverseInteger.py
@@ -1,2 +1,11 @@
-x = int("".join(reversed(str(123450))))
-print(x)
+class Solution:
+    def reverse(self, x: int) -> int:
+        reverse = int("".join(reversed(str(abs(x)))))
+        if x < 0:
+            reverse = -abs(reverse)
+        min = -2147483648
+        max = 2147483647
+        if reverse >= min and reverse <= max:
+            return reverse
+        else:
+            return 0
