12f4534e1db084925a7a7e358f7c0b851b347d90
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-07 10:55:53 +1000

    Create Easy-RomantoInteger.py

---
+++ Easy-RomantoInteger.py
@@ -0,0 +1,10 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        array = []
+        for i in s:
+            array.append(i)
+        print(array)
+
+sol = Solution()
+
+sol.romanToInt('II')
