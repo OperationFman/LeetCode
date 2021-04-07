5a6365eec1d9768bc2ed2dc3166fe5f36c0cfead
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-07 11:24:26 +1000

    Update Easy-RomantoInteger.py

--- Easy-RomantoInteger.py
+++ Easy-RomantoInteger.py
@@ -4,9 +4,19 @@ class Solution:
         result = 0
         for k in s:
             array.append(k)
+
+        if array[0] == 'I': result = result + 1
+        if array[0] == 'V': result = result + 5
+        if array[0] == 'X': result = result + 10
+        if array[0] == 'L': result = result + 50
+        if array[0] == 'C': result = result + 100
+        if array[0] == 'D': result = result + 500
+        if array[0] == 'M': result = result + 1000
+
         for i in range(len(array)-1):
-            """Looking for I, IV and IX"""
-            if array[i] == 'I' and array[i+1] == 'V':
+            if i == 0:
+                continue
+            elif array[i] == 'I' and array[i+1] == 'V':
                 result = result + 4
             elif array[i] == 'I' and array[i+1] == 'X':
                 result = result + 9
@@ -22,4 +32,4 @@ class Solution:
 
 sol = Solution()
 
-print(sol.romanToInt('V'))
+print(sol.romanToInt('IIe'))
