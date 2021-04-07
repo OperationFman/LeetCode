4e0b49a79bec7a5281f50eb527b96a895df0df61
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-07 11:16:29 +1000

    Update Easy-RomantoInteger.py

--- Easy-RomantoInteger.py
+++ Easy-RomantoInteger.py
@@ -1,10 +1,25 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         array = []
-        for i in s:
-            array.append(i)
-        print(array)
+        result = 0
+        for k in s:
+            array.append(k)
+        for i in range(len(array)-1):
+            """Looking for I, IV and IX"""
+            if array[i] == 'I' and array[i+1] == 'V':
+                result = result + 4
+            elif array[i] == 'I' and array[i+1] == 'X':
+                result = result + 9
+            elif array[i] == 'I':
+                result = result + 1
+            elif i == 0 and array[i] == 'V':
+                result = result + 5
+            elif i != 0:
+                if array[i] == 'V' and array[i-1] != 'I':
+                    result = result + 5
+        # Don't forget to add the last roman numeral missed in the above loop
+        return result
 
 sol = Solution()
 
-sol.romanToInt('II')
+print(sol.romanToInt('V'))
