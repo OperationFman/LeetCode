180837a0681f45b4be75e4e5912d50eca2caeeb2
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-07 14:04:01 +1000

    Fixed and finished

--- EasyRomantoInteger.py
+++ EasyRomantoInteger.py
@@ -1,38 +1,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        array = []
         result = 0
-        for k in s:
-            array.append(k)
-
-
-        for i in range(len(array)-1):
-            """Upto L"""
-            # Upper Checks
-            if array[i] == 'I' and array[i+1] == 'V':
-                result = result + 4
-            elif array[i] == 'I' and array[i+1] == 'X':
-                result = result + 9
-            # Single Check
-            elif i == 0:
-                if array[0] == 'I': result = result + 1
-                if array[0] == 'V': result = result + 5
-                if array[0] == 'X': result = result + 10
-                if array[0] == 'L': result = result + 50
-                if array[0] == 'C': result = result + 100
-                if array[0] == 'D': result = result + 500
-                if array[0] == 'M': result = result + 1000
-            elif array[i] == 'I':
-                result = result + 1
-            # Lower Checks
-            elif i != 0:
-                if array[i] == 'V' and array[i-1] != 'I':
-                    result = result + 5
-                elif array[i] == 'X' and array[i-1] != 'I':
-                    result = result + 10
-        # Don't forget to add the last roman numeral missed in the above loop
+        array = { 'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        count = array[s[0]]
+        for _, i in enumerate(s):
+            if array[i] > count:
+                result = result + array[i]
+                result = result - count * 2
+                count = array[i]
+            else:
+                result += array[i]
+                count = array[i]
         return result
-
-sol = Solution()
-
-print(sol.romanToInt('IXIVe'))
--- test_RomantoInteger.py
+++ test_RomantoInteger.py
@@ -2,42 +2,42 @@ from EasyRomantoInteger import Solution
 
 def test_I():
     sol = Solution()
-    assert sol.romanToInt('Ie') == 1
+    assert sol.romanToInt('I') == 1
 
 def test_II():
     sol = Solution()
-    assert sol.romanToInt('IIe') == 2
+    assert sol.romanToInt('II') == 2
 
 
 
 
 def test_IV():
     sol = Solution()
-    assert sol.romanToInt('IVe') == 4
+    assert sol.romanToInt('IV') == 4
 def test_IVIVIV():
     sol = Solution()
-    assert sol.romanToInt('IVIVIVe') == 12
+    assert sol.romanToInt('IVIVIV') == 12
 def test_IVIIVIVI():
     sol = Solution()
-    assert sol.romanToInt('IVIIVIVIe') == 14
+    assert sol.romanToInt('IVIIVIVI') == 14
 
 def test_IX():
     sol = Solution()
-    assert sol.romanToInt('IXe') == 9
+    assert sol.romanToInt('IX') == 9
 def test_IXIXIX():
     sol = Solution()
-    assert sol.romanToInt('IXIXIXe') == 27
+    assert sol.romanToInt('IXIXIX') == 27
 def test_IXIVIVIXI():
     sol = Solution()
-    assert sol.romanToInt('IXIVIVIXIe') == 27
+    assert sol.romanToInt('IXIVIVIXI') == 27
 
 
 
 
 def test_V():
     sol = Solution()
-    assert sol.romanToInt('Ve') == 5
+    assert sol.romanToInt('V') == 5
 
 def test_X():
     sol = Solution()
-    assert sol.romanToInt('Xe') == 10
+    assert sol.romanToInt('X') == 10
