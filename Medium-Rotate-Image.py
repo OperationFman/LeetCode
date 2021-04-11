3a971c5dff54ee69d63c59cf78a7d978384a873d
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-11 10:05:11 +1000

    Update Medium-Rotate-Image.py

--- Medium-Rotate-Image.py
+++ Medium-Rotate-Image.py
@@ -1,4 +1,7 @@
-matrix = [[1,2], [3,4]]
+matrix = [[1,2], [3,4],[5,6]]
 
-matrix[0].insert(0, 9)
-print(matrix)
+# matrix[0].insert(0, 9)
+
+# print(matrix)
+# for i in range(len(matrix)):
+#     print(i)
