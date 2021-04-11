87866aaffd6ee5971675b655111b03ea7cd4b92c
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-11 10:07:22 +1000

    Update Medium-Rotate-Image.py

--- Medium-Rotate-Image.py
+++ Medium-Rotate-Image.py
@@ -5,3 +5,16 @@ matrix = [[1,2], [3,4],[5,6]]
 # print(matrix)
 # for i in range(len(matrix)):
 #     print(i)
+
+array = []
+for i in range(5):
+    array.append([])
+
+print(array)
+
+# newarray = []
+        # for i in range(len(matrix))
+        # newarray.append([])
+        # for element in array[i]
+        #    newarray[array.index(element)].insert(0, element)
+        # matrix = newarray
