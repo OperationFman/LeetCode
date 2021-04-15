33dd294fa5feb2ca264b21404024fced8671d09b
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-15 16:17:16 +1000

    Update Easy-2DArray.py

--- Easy/Easy-2DArray.py
+++ Easy/Easy-2DArray.py
@@ -6,3 +6,9 @@ def hourglassSum(arr):
             hourglass.append(arr[col][row]+arr[col][row+1]+arr[col][row+2]+arr[col+1][row+1]+arr[col+2][row]+arr[col+2][row+1]+arr[col+2][row+2])    
     result = max(hourglass)
     return max(result)
+
+# To make an empty 2D array easily:
+rows = 8
+cols = 8
+array = [[0]*cols]*rows
+print(array)
