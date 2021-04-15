eb1450c0a7300873626fe12e982a5af322a49a69
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-15 16:03:09 +1000

    Update Easy-Fibonacci.py

--- Easy/Easy-Fibonacci.py
+++ Easy/Easy-Fibonacci.py
@@ -3,7 +3,6 @@
 # Each term is the sum of the previous 2 terms
 # So a fib number of 10 would equal 13, why? idk
 
-# Slow way
 def fibonacci(n):
     if n == 1:
         return 1
@@ -11,6 +10,7 @@ def fibonacci(n):
         return 2
     elif n > 2:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
 print(fibonacci(6))
 for i in range(1, 20):
     print(i, ":", fibonacci(i))
