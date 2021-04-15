cc565a1683f6fa4cd7f2966c2e51b7385c7b7c50
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-15 16:46:57 +1000

    Create Easy-Reotate-Matrix.py

---
+++ Easy/Easy-Reotate-Matrix.py
@@ -0,0 +1,10 @@
+def transpose(matrix):
+    cols = len(matrix)
+    rows = len(matrix[0])
+    array = [[0 for _ in range(rows)] for __ in range(cols)]
+    for i in range(rows):
+        for j in range(cols):
+            array[j][i] = matrix[i][j]
+    return array
+
+print(transpose([[1,2,3],[4,5,6],[7,8,9]]))
