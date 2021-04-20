8a81c55a6764db1467619ad8fcf9bee41d5405d0
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-20 16:47:07 +1000

    Create Highest-Lowest.py

---
+++ Easy/Highest-Lowest.py
@@ -0,0 +1,9 @@
+def high_and_low(numbers):
+    numbers = numbers.split(" ")
+    array = []
+    for i in numbers:
+        array.append(int(i))
+    array.sort()
+    l, h = array[0], array[-1]
+    numbers = str(h) + " " + str(l)
+    return numbers
