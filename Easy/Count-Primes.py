09cfd1c48432bcc03b8b66b5746aacfd1cf72dd2
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-23 15:01:53 +1000

    Create Count-Primes.py

---
+++ Easy/Count-Primes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+        res = [1] * n
+        res[0] = 0
+        res[1] = 0
+        for i in range(2, int(sqrt(n)) + 1):
+            if res[i] == 1:
+                for j in range(i + i, n, i):
+                    res[j] = 0
+        return sum(res)
