1a134571e3895ea9b3bd3fa5c4463c337f29b478
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-30 12:21:32 +1000

    Create 1-Reverse-String.py

---
+++ Reinforcement/1-Reverse-String.py
@@ -0,0 +1,8 @@
+# Input 1: ["h","e","l","l","o"]
+
+class Solution:
+    def reverseString(self, s):
+        s.reverse() # ["o","l","l","e","h"]
+
+# Reverse method does what you'd expect
+# This questions doesn't want a return, only an edit in place
