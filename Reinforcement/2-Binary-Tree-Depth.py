8bac4a21488ae5b40ab855b935e9aec4334d30f4
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-03 14:24:06 +1000

    Added notes

--- Reinforcement/2-Binary-Tree-Depth.py
+++ Reinforcement/2-Binary-Tree-Depth.py
@@ -5,18 +5,19 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def maxDepth(self, root): # The tree is already passed in
+    def maxDepth(self, root): # The tree is already passed in as the below green text
         """ TreeNode{val: 3, left: TreeNode{val: 9, left: None, right: None}, 
             right: TreeNode{val: 20, left: TreeNode{val: 15, left: None, right: None}, 
             right: TreeNode{val: 7, left: None, right: None}}} """
-        if root != None: 
-            return self._height(root, 0)
+        if root != None:                 # Starts the following method with 0.
+            return self._height(root, 0) # The result with be a single int of the longest branch.
         else:
-            return 0
+            return 0 #Edge Case: Could be empty, so 0.
         
-    def _height(self, cur, cur_height):
-        if cur == None:
+    def _height(self, cur, cur_height): #2. Left node is now main node, height incremented by 1
+        if cur == None: # 3. Node could be last one so return the height
             return cur_height
-        left_height = self._height(cur.left, cur_height + 1)
-        right_height = self._height(cur.right, cur_height + 1)
-        return max(left_height, right_height)
+        left_height = self._height(cur.left, cur_height + 1) # 1. Passes in remaining tree from left node
+        right_height = self._height(cur.right, cur_height + 1) # 4. Runs aswell, all branches will be travelled
+        return max(left_height, right_height) # Will return current longest, when moving up itll report the enxt longest etc
+        # Works all the way back upto the first left & right nodes with longest branch only
