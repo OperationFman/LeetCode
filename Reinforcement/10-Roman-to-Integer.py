26b0e29ff72dc0190e962bd3e0ae7f7db9b5242c
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-08 08:53:45 +1000

    Update 10-Roman-to-Integer.py

--- Reinforcement/10-Roman-to-Integer.py
+++ Reinforcement/10-Roman-to-Integer.py
@@ -2,7 +2,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         result = 0
         array = { 'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        count = array[s[0]]
+        count = array[s[0]
         for _, i in enumerate(s):
             if array[i] > count:
                 result = result + array[i]
