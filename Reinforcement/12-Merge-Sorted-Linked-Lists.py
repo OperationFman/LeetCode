844dccc4199d7d15cb0c260c6a9244780bdb3a17
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-08 09:33:42 +1000

    Update 12-Merge-Sorted-Linked-Lists.py

--- Reinforcement/12-Merge-Sorted-Linked-Lists.py
+++ Reinforcement/12-Merge-Sorted-Linked-Lists.py
@@ -14,34 +14,34 @@ class Solution:
             elif l1 != None and l2 == None:
                 return l1
             
-            array1 = []
-            array2 = []
+            array1 = [] # Using arrays as it's easier to sort
+            array2 = [] # Also easier to just fill them, then iterate over them making a new Linkedlist
 
-            cur = l1
+            cur = l1 # Cursor to start at l1
 
-            array1.append(cur.val)
-            while cur.next != None:
-                array1.append(cur.next.val)
+            array1.append(cur.val) # First element gets skipped because we only use the 'next' value, so here we add it manually
+            while cur.next != None: # Loop stops at the end of the LL
+                array1.append(cur.next.val) # Put the value of next in the array and move down the LL
                 cur = cur.next
 
-            cur = l2
+            cur = l2 # Does exactly the same as previous block on the second array
             array2.append(cur.val)
             while cur.next != None:
                 array2.append(cur.next.val)
                 cur = cur.next
 
-            result = sorted(array1 + array2)
+            result = sorted(array1 + array2) # Much easier and more readable to sort arrays this way and store as a new array
 
-            newList = ListNode()
+            newList = ListNode() # new instance of a linkedlist
             cur = newList
-            cur.val = result.pop(0)
+            cur.val = result.pop(0) # Similar to reading the LLs, the first would be missed to we add it manually. Also, by popping we can iterate over the remaining array easily
 
             for i in result:
-                cur.next = ListNode()
-                cur.next.val = i
-                cur = cur.next
+                cur.next = ListNode() # Create a node and add it to the next
+                cur.next.val = i # Change the values of said node
+                cur = cur.next # Move down the LL
 
-            return newList
+            return newList 
         except:
-            node = ListNode()
-            return node
+            node = ListNode() # Likely if something went wrong it was because it's an empty node, right after the try we catch 'nothing' but not a node with nothing
+            return node # Two empty nodes would equal an empty node so might aswell catch and return it
