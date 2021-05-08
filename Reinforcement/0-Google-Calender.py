cefefb1e190d2e387433b4f35844c3e5fb3fcb48
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-09 09:34:44 +1000

    Setup

--- Reinforcement/0-Google-Calender.py
+++ Reinforcement/0-Google-Calender.py
@@ -3,3 +3,7 @@
 
 # Input 2 - [["10:00", "11:30"], ["12:30", "14:30"], ["14:30, 15:00"], ["16:00", "17:00"]]
 # Bounds - ["10:00", "18:30"]
+
+# Return the 'blocks' available in both calenders for a meeting, e.g:
+# [[11:30, 12:00], [15:00, 16:00], [17:00, 18:30]]
+# A block is a minimum of 30 minutes but must not be secular 30's
