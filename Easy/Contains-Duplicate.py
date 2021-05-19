a5ac8eefe154f33209c458309fb80289cbc55a13
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-19 17:38:42 +1000

    Update Contains-Duplicate.py

--- Easy/Contains-Duplicate.py
+++ Easy/Contains-Duplicate.py
@@ -1,5 +1,5 @@
 class Solution:
     def containsDuplicate(self, nums):
-        if len(nums) != len(set(nums)):
-            return True
-        return False
+        if len(nums) != len(set(nums)): #When we make the list unique (set) it'll remove duplicates
+            return True # If that's less than originally then it'll mean there was dups in it
+        return False # If it's the same then it must have been unique
