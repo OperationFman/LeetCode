211f38ec4be4e7d7c05364ca83c558ef87f3530f
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-23 13:13:36 +1000

    Update PalindromeMissingCharacters.py

--- Medium/PalindromeMissingCharacters.py
+++ Medium/PalindromeMissingCharacters.py
@@ -3,22 +3,22 @@ def Solution(S):
         S = list(S)
         copy = S[::-1] # O(n)
 
-        for i in range(len((S))):
+        for i in range(len((S))): # one iteration, O(n)
             if S[i] == '?' and copy[i] == '?':
-                S[i], copy[i] = 'a', 'a'
-            elif S[i] == '?' and copy[i] != '?':
+                S[i], copy[i] = 'a', 'a' # If both questions marks then make both m,atch with 'a'
+            elif S[i] == '?' and copy[i] != '?': # If one a question mark, make it match the other
                 S[i] = copy[i]
-            elif S[i] != '?' and copy[i] == '?':
+            elif S[i] != '?' and copy[i] == '?': 
                 copy[i] = S[i]
-            elif S[i] != copy[i]:
+            elif S[i] != copy[i]: # If they're letters but dont match, it's invalid
                 return "NO"
             else:
-                continue
+                continue # If they're the same it''l continue
 
-        if S == copy:
+        if S == copy: # Palindromes match when reversed, if they do convert to string and return
             result = ''.join([str(elem) for elem in copy])
             return result
         else:
             return "NO"
     except:
-        return "NO"
+        return "NO" # Something couldve gone wrong, catch it (e.g empty String)
