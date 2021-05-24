7403879b209d6f492c3110ba05efe2c772ae9976
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-24 12:23:52 +1000

    Update stalinSort.py

--- Easy/stalinSort.py
+++ Easy/stalinSort.py
@@ -1,10 +1,11 @@
 array = [1, 6, 2, 4, 8, 1, 4, 3, 6, 3, 8, 3, 4, 6, 9, 1, 3, 6, 2, 9, 5, 7, 2, 4, 6]
 
 def stalinSort(array):
-    elims = []
     count = array[0]
-    for i in array:
-        if i != count:
+    elims = []
+    
+    for i, v in enumerate(array):
+        if v != count:
             elims.append(i)
         else:
             count += 1
