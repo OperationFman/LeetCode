0056d3e7ea66481e5d8080bdf6f873e39b6d18fd
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-25 14:42:22 +1000

    Update Product-of-Array-Except-Self.py

--- Medium/Product-of-Array-Except-Self.py
+++ Medium/Product-of-Array-Except-Self.py
@@ -1,9 +1,9 @@
-import numpy # 
+import numpy # Has a method that allows acquiring the product of a list
 
 class Solution:
     def productExceptSelf(self, nums):
         result = []
-        for i, v in enumerate(nums):
+        for i, v in enumerate(nums): #Classic, remove item from list, do action on the list, put it back
             nums.pop(i)
             result.append(numpy.prod(nums))
             nums.insert(i, v)
