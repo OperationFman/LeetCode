2ec42d29142b1bcbb1e90a8a786c236cb18d08b1
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-26 20:07:58 +1000

    Update Min-Stack.py

--- Easy/Min-Stack.py
+++ Easy/Min-Stack.py
@@ -4,24 +4,24 @@ class MinStack:
         """
         initialize your data structure here.
         """
-        self.array = []
+        self.array = [] # Need a data structure, array is the simplest but not the best, nor is it a stack
         
 
     def push(self, val: int) -> None:
-        self.array.append(val)
+        self.array.append(val) # Adds value onto the end of the stack. O(1)
         
 
     def pop(self) -> None:
-        self.array.pop(-1)
+        self.array.pop(-1) # array[-1] is O(1) since nothing before it needs replacing
         
 
     def top(self) -> int:
-        return self.array[-1]
+        return self.array[-1] # Also O(1)
         
 
     def getMin(self) -> int:
-        result = self.array[0]
-        for i in self.array:
+        result = self.array[0] # Set value to the first in the list, everything after that should be able to change it or it stays as is
+        for i in self.array: # O(n)
             if i < result:
-                result = i
+                result = i # Change the result if it's lower
         return result
