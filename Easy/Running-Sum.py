91215386f0379a11f76cb4de7afdd7fb0bcaf46f
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-28 15:36:49 +1000

    Update Running-Sum.py

--- Easy/Running-Sum.py
+++ Easy/Running-Sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def runningSum(self, nums):
         result = []
-        runner = 0
+        runner = 0 # Quicker to capture a total to add to rather than recalc the sum every time
+
         for i in nums:
-            runner += i
-            result.append(runner)
+            runner += i # Increment the runner to get the new value
+            result.append(runner) # Add that value to the array
         return result
