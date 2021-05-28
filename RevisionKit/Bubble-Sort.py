b786ea717103bdff544cc1e351499fef5145b046
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-28 11:40:38 +1000

    Update Bubble-Sort.py

--- RevisionKit/Bubble-Sort.py
+++ RevisionKit/Bubble-Sort.py
@@ -8,7 +8,17 @@
 
 def countSwaps(a):
   """ [2, 1, 3] """
-    # Clue: Follow the traditional method of bubble sort and run a counter for each swap. ensure O(n)
-  sorty = a.sort()
-  while a != sorty:
-    
+    # Clue: Follow the traditional method of bubble sort and run a counter for each swap.
+  count = 0
+  for i in range(len(a) - 1):
+    for x in range(len(a) - i - 1):
+      if a[x] > a[x + 1]:
+        a[x], a[x + 1] = a[x + 1], a[x]
+        count += 1
+  
+  print("Array is sorted in " + str(count) + " swaps")
+  print("First Element: " + str(a[0]))
+  print("Last Element: " + str(a[-1]))
+
+
+countSwaps([1,2,3,5,4,6,1,7,2])
