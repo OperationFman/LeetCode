ab38c30dfc052980db305f1dcac55b1c685f00f8
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-28 14:38:10 +1000

    Update arrayMaxConsecutiveSum2.py

--- CodeSignal/arrayMaxConsecutiveSum2.py
+++ CodeSignal/arrayMaxConsecutiveSum2.py
@@ -1,14 +1,14 @@
 def arrayMaxConsecutiveSum2(inputArray):
-    """ [-2, 2, 5, -11, 6] """
-    maxSum = inputArray.pop(0)
-    currSum = maxSum
+    """ [2, 5, -11, 6] """
+    maxSum = inputArray.pop(0) # 2
+    currSum = maxSum # 2
     
-    for i in inputArray:
-        if i + currSum > i:
-            currSum += i
+    for i in inputArray: # 
+        if i + currSum > i: #  
+            currSum += i 
         else:
-            currSum = i
-        maxSum = max(maxSum, currSum)
+            currSum = i # 
+        maxSum = max(maxSum, currSum) # 
     maxSum = max(maxSum, currSum)
     
     return maxSum
