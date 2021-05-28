43bc9c912072b3012489cd2a19e11f071ff610ee
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-28 14:58:24 +1000

    Update strStr.py

--- Easy/strStr.py
+++ Easy/strStr.py
@@ -1,3 +1,3 @@
 class Solution:
     def strStr(self, haystack, needle):
-        return haystack.find(needle) # Retuerns lowest index of the substring, -1 is it can;t find it
+        return haystack.find(needle) # Retuerns lowest index of the substring, -1 is it can't find it
