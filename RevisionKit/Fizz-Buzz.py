673649b69cb997cd01375fda106a944cd592ff28
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-28 13:28:21 +1000

    Update Fizz-Buzz.py

--- RevisionKit/Fizz-Buzz.py
+++ RevisionKit/Fizz-Buzz.py
@@ -1,4 +1,13 @@
 class Solution:
-    def fizzBuzz(self, n):
-        pass
-    # You can do this :)
+    def fizzBuzz(n):
+        for i in range(1, n):
+            if i % 15 == 0:
+                print("FizzBuzz")
+            elif i % 5 == 0:
+                print("Buzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            else:
+                print(i)
+
+Solution.fizzBuzz(100)
