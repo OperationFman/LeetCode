7749ce0e2979aee5ae8135313ce68868c09ec1fd
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-29 11:27:03 +1000

    Update Left-Rotation.py

--- RevisionKit/Left-Rotation.py
+++ RevisionKit/Left-Rotation.py
@@ -8,7 +8,12 @@ def rotLeft(a, d):
     for _ in range(d):
         a.append(a[0]) # Put first element on the back
         a.pop(0) # Delete first element
-        
     return a
 
 rotLeft([1, 2, 3, 4, 5], 4) # Rotate array 4 times
+
+def rotRight(a, d):
+    for _ in range(d):
+        a.insert(0,a[-1])
+        a.pop(-1)
+    return a
