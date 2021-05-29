9cf242ca675ceac6418688017ae553d264ed08f8
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-29 14:50:00 +1000

    Update Ransom-Note.py

--- RevisionKit/Ransom-Note.py
+++ RevisionKit/Ransom-Note.py
@@ -1,3 +1,12 @@
 def checkMagazine(magazine, note):
     """ [['Quick'], ['Fox']] & [['Quick'], ['Brown'] ['Fox']] """
     # Clue: Once you use a magazine word, aka element, it is removed and can't be used again. pop is your friend
+    output = "Yes"
+    
+    for i in note:
+        if i in magazine:
+            magazine.pop(magazine.index(i))
+        else:
+            output = "No"
+    
+    print(output)
