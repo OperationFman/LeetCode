7b87a13d8b8d0d536e6304033da7840dc4c2d01e
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-29 11:34:34 +1000

    Update Majority-Element.py

--- RevisionKit/Majority-Element.py
+++ RevisionKit/Majority-Element.py
@@ -1,12 +1,11 @@
 # Given an array nums of size n, return the majority element.
 from collections import Counter
 
-
-class Solution:
-    def majorityElement(nums):
+def majorityElement(nums):
     # You know how to use Counter for this
-        count = Counter(nums).most_common()
-        print(count[0][0])
+    count = Counter(nums).most_common()
+    print(count[0][0])
 
-Solution().majorityElement([1,2,2,3])
+array = [1,2,2,3,3,3,3,3]
+majorityElement(array)
         
