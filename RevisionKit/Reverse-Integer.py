4b954a6b997c6d7cc7affaba11e6fbc8d19141ae
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-29 14:59:46 +1000

    Delete and change

--- RevisionKit/Reverse-Integer.py
+++
@@ -1,8 +0,0 @@
-# Given a signed 32-bit integer x (aka between 2147483648 and -2147483648), return x with its digits reversed. 
-# If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0
-
-class Solution:
-    def reverse(self, x):
-        pass
-        # Clue: Convert to a string, reverse, convert back and return. If it's negative, store that ahead of time then turn 
-        # it back to negative at the end. Then simply check it's in range
--- RevisionKit/Single-Number.py
+++ RevisionKit/Single-Number.py
@@ -1,6 +1,13 @@
-# Given a not-empty array, one element isn;t repeated. Find it
+# Given a not-empty array, one element isn't repeated. Find it
 
+from collections import Counter
 class Solution:
     def singleNumber(self, nums):
-        pass
         # Do the classic: enumerate, pop, check, insert if not
+        count = Counter(nums)
+        for i in count:
+            if count[i] == 1:
+                return i
+
+
+print(Solution().singleNumber([1,1,2,2,3,3,4,5,5,6,6]))
