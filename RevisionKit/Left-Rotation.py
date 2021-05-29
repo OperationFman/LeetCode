0b0d614f59c9fb1feefacda5dfd2dd22f3c09f2a
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-29 11:20:30 +1000

    Update Left-Rotation.py

--- RevisionKit/Left-Rotation.py
+++ RevisionKit/Left-Rotation.py
@@ -4,6 +4,12 @@
 # [3, 4, 5, 1, 2]
 # [4, 5, 1, 2, 3]
 
-def rotateLeft(d, arr):
-    pass
+def rotateLeft(arr):
     # Clue Use a for loop with no value, eg for _ in range(x). Just for practice
+    zip(*arr)
+    print(arr)
+
+rotateLeft([[1, 2, 3, 4, 5],
+            [2, 3, 4, 5, 1], 
+            [3, 4, 5, 1, 2], 
+            [4, 5, 1, 2, 3]])
