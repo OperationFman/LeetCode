9193b4bb68a60c3d4f04de8ba21b971e4e16e96a
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-30 10:02:19 +1000

    Update Two-Sum.py

--- RevisionKit/Two-Sum.py
+++ RevisionKit/Two-Sum.py
@@ -2,6 +2,7 @@
 
 class Solution:
     def twoSum(self, nums, target):
-        pass
-        # Originally you used a nested for loop O(n^2), try find a new way
-        # Originally you iterated and ensuring the indexes wernt the same (To prevent the same value being calculated twice)
+        for i, v in enumerate(nums):
+            for j, k in enumerate(nums):
+                if v + k == target and i != j:
+                    return [i, j]
